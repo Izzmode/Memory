e301c49e0844521958ba4cd30f209493e4edd835
Izabelle Olofsson <izabelleolofsson@live.se>
2023-01-14 23:21:48 +0100

    basics are done, fixed bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const section = document.querySelector('section');
 const playerLivesCount = document.querySelector('.playerLivesCount');
-const playerLives = 6;
+let playerLives = 6;
 
 playerLivesCount.textContent = playerLives;
 
@@ -39,7 +39,7 @@ const randomize = () => {
 const cardGenerator = () =>{
     const cardData = randomize();
     console.log(cardData);
-
+    
     cardData.forEach(item => {
         const card = document.createElement('div');
         card.classList = 'card';
@@ -49,6 +49,7 @@ const cardGenerator = () =>{
         back.classList = 'back';
 
         face.src = item.imgSrc;
+        card.setAttribute('name', item.name);
 
         section.appendChild(card);
         card.appendChild(face);
@@ -56,39 +57,76 @@ const cardGenerator = () =>{
 
         card.addEventListener('click', e => {
             card.classList.toggle('toggleCard');
+            checkCards(e);
         })
     })
 
    
+    const checkCards = (e) => {
+        const clickedCard = e.target;
+        console.log(clickedCard);
+        clickedCard.classList.add('flipped');
+        const flippedCards = document.querySelectorAll('.flipped');
+        const toggleCard = document.querySelectorAll('.toggleCard');
+        
+
+        if(flippedCards.length === 2) {
+            if(flippedCards[0].getAttribute('name') === flippedCards[1].getAttribute('name')){
+                console.log('match')
+                flippedCards.forEach(card => {
+                    card.classList.remove('flipped');
+                    card.style.pointerEvents = 'none';
+                });
+            } 
+            else{
+                console.log('wrong')
+                flippedCards.forEach(card => {
+                    card.classList.remove('flipped');
+                    
+                    setTimeout( () => card.classList.remove('toggleCard'), 1000);
+                });
+
+                playerLives--;
+                playerLivesCount.textContent = playerLives;
+                if(playerLives == 0){
+                    restart('try again');
+                };
+            } 
+        }
+    
     
 
+        if(toggleCard.length === 16){
+        restart('You won')
+        };
+    }
+};
+
+
+const restart = (text) => {
+    let cardData = randomize();
+    let faces = document.querySelectorAll('.face')
+    let cards = document.querySelectorAll('.card')
+    section.style.pointerEvents ='none';
+
+    cardData.forEach((item, index) => {
+        cards[index].classList.remove('toggleCard');
+      setTimeout(() => {
+        cards[index].style.pointerEvents = 'all';
+        faces[index].src = item.imgSrc;
+        cards[index].setAttribute('name', item.name);
+        section.style.pointerEvents ='all';
+      }, 1000);
+    });
+
+    playerLives = 6;
+    
+    playerLivesCount.textContent = playerLives;
+
+    setTimeout(() => window.alert(text), 100);
 }
 
 cardGenerator();
-
-
-
-
-
-// const squares = document.querySelectorAll('.square');
-
-
-
-// const myFunction = e =>{
-//     // alert('funkar det nu?')
-//     console.log(e.target)
-
-//     if (!e.target.classList.contains('square')){
-//         return
-//     }
-
-//     e.target.classList.toggle('active')
-// };
-
-// squares.forEach(square =>{
-//     addEventListener('click', myFunction)
-    
-// })
 
 
 
